5d86cd317b653c42ef01f1bcae68637d5f360d5a
matthewpincus <30541540+matthewpincus@users.noreply.github.com>
2024-09-01 21:34:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let score = 0;
 let gameInterval;
-const captainPlanet = document.getElementById('captain-planet');
+let captainPlanet;
 const gameArea = document.getElementById('game-area');
 const scoreDisplay = document.getElementById('score-value');
 const startButton = document.getElementById('start-button');
